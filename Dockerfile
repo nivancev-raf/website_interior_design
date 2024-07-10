103f02eccee5cd2c258efe7b5aba298a91a9f534
nivancev-raf <nivancev5321rn@raf.rs>
2024-07-10 18:27:28 +0200

    updated pictures and email sending

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ COPY . .
 RUN mvn clean package -DskipTests
 
 FROM openjdk:17.0.1-jdk-slim 
+RUN apt-get update && apt-get install -y locales && locale-gen sr_RS.UTF-8
+ENV LANG sr_RS.UTF-8
+ENV LANGUAGE sr_RS:sr
+ENV LC_ALL sr_RS.UTF-8
 COPY --from=build /target/Notifikacioni_servis-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8081
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/nikolalukatrening/Notifikacioni_servis/service/impl/EmailServiceImpl.java
+++ src/main/java/nikolalukatrening/Notifikacioni_servis/service/impl/EmailServiceImpl.java
@@ -2,37 +2,51 @@ package nikolalukatrening.Notifikacioni_servis.service.impl;
 
 import nikolalukatrening.Notifikacioni_servis.dto.EmailMessageDto;
 import nikolalukatrening.Notifikacioni_servis.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
-    @Autowired
-    private JavaMailSender emailSender;
+    private final JavaMailSender emailSender;
+
+    public EmailServiceImpl(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
 
     @Override
     public void sendEmail(EmailMessageDto emailMessage) {
-        JavaMailSenderImpl mailSenderImpl = (JavaMailSenderImpl) emailSender;
-        mailSenderImpl.getJavaMailProperties().put("mail.smtp.ssl.trust", "*");
+        MimeMessage message = emailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
+            helper.setFrom("nivancev02@gmail.com");
+            helper.setTo(emailMessage.getEmail());
+            helper.setSubject(emailMessage.getSubject());
 
-        // treba pogledati kako za name
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("nivancev02@gmail.com");
-        message.setTo(emailMessage.getEmail());
-//        message.setTo("nivancev02@gmail.com");
-//        System.out.println("email od:" + emailMessage.getEmail());
-//        message.setFrom(emailMessage.getEmail());
-        message.setSubject(emailMessage.getSubject());
+            String htmlContent = "<html><body>"
+                    + "<h1 style='color: navy;'>Max Design</h1>"
+                    + "<p>Poštovani " + emailMessage.getName() + ",</p>"
+                    + "<p>Hvala Vam što ste nas kontaktirali. Vaša poruka jeuspešno poslata sa sledećim sadržajem: </p>"
+                    + "<br>"
+                    + "<h3 style='color: navy;'>" + emailMessage.getSubject() + "</h3>"
+                    + "<p><b>Name:</b> " + emailMessage.getName() + "</p>"
+                    + "<p><b>Email:</b> " + emailMessage.getEmail() + "</p>"
+                    + "<p><b>Message:</b></p><p>" + emailMessage.getMessage() + "</p>"
+                    + "<p style='color: grey;'>Očekujte odgovor u najkraćem mogućem roku. Hvala!</p>"
+                    + "<br>"
+                    + "<p>Srdačan pozdrav,</p>"
+                    + "<p>Max Design</p>"
+                    + "</body></html>";
 
-        String mailMessage = "Vasa poruka je uspešno poslata sa sledećim sadržajem: " + "\n" + "\n" + emailMessage.getMessage() + "\n" + "\n" + "Ocekujte odgovor u najkracem mogucem roku. Hvala!";
-        message.setText(mailMessage);
-        mailSenderImpl.send(message);
+            helper.setText(htmlContent, true); // true indicates HTML
+            // continue with sending the email...
+            emailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
-
-
